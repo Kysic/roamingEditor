2a40c89db34592d8f84d899437e0156fcebd32d1
Kysic <ludocourrier-github@yahoo.fr>
2017-11-10 19:09:24 +0100

    Mise à jour page d'accueil

--- index.php
+++ index.php
@@ -1,13 +1,39 @@
-<?php
-header('Location: portal');
-?>
 <!doctype html>
 <html lang="fr">
 <head>
-  <meta charset="utf-8">
-  <title>Editeur de comptes rendus de maraudes</title>
+<meta charset="utf-8">
+<link rel="icon" type="image/png" href="img/favicon.png" />
+<title>Accueil bénévoles VINCI</title>
+<style>
+body {
+    text-align: center;
+    color: #d9dadc;
+    background: #3d4045;
+    padding: 20px;
+}
+a {
+    color: #d9dadc;
+    font-weight: bold;
+    text-decoration: none;
+}
+a:hover {
+    color: #fff;
+    text-decoration: underline;
+}
+</style>
 </head>
 <body>
-  <a href='portal'>Les comptes rendus de maraudes</a>
+<h1>Bienvenue sur ce site dédié aux bénévoles du VINCI</h1>
+<br/>
+<p>
+<a href="dokuwiki">Pour accéder au WIKI contenant pleins d'infos, cliquez ici.</a>
+</p>
+<p>
+<a href="portal">Et pour consulter les comptes rendus de maraude, cliquez ici.</a>
+</p>
+<br/>
+<p>
+<a href="https://www.samu-social-grenoble.fr/"><img src="img/logo-SamuSocialGrenoble-Negatif.svg" width="250px"/></a>
+</p>
 </body>
 </html>
